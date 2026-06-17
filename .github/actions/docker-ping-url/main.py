a2544df7c408af88108b7305591e7e55b8045f17
maytheforce-be-with-you <saylis.work@gmail.com>
2026-06-17 21:20:56 +0000

    feat(17-3-custom-docker.yaml):finish ping url script

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,2 +1,34 @@
-if __name__ == "__main__":
+import os
+import requests
+import time
+def pingUrl(url, delay, max_trails):
+    trials = 0
+    while trials < max_trails:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"website {url} is reachable")
+                return True
+        except requests.ConnectionError:
+            print(f"{url} is unreachable. Retrying in {delay} seconds")
+            time.sleep(delay)
+            trials += 1
+        except requests.missingSchema:
+            print(f"wrong schema {url}")
+            return False
+
+def run():
     print("Hello world")
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRAIALS"))
+
+    website_reachable = pingUrl(website_url, delay, max_trials)
+
+    if not website_reachable:
+        raise Exception(f"website{website_url} is malformed or not reachable")
+    
+    print(f"website {website_url} is reachable")   
+
+if __name__ == "__main__":
+    run();
