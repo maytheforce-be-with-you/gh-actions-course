5088e65c1dfd42eaf0019b1be1a39633f038e300
maytheforce-be-with-you <saylis.work@gmail.com>
2026-06-17 21:25:31 +0000

    feat(17-3-custom-docker.yaml):finish ping url script

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -21,7 +21,7 @@ def run():
     print("Hello world")
     website_url = os.getenv("INPUT_URL")
     delay = int(os.getenv("INPUT_DELAY"))
-    max_trials = int(os.getenv("INPUT_MAX_TRAIALS"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
 
     website_reachable = pingUrl(website_url, delay, max_trials)
 
